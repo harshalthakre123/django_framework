7b970cb35eebaabdb36e4e4e9f810883ba0124f5
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-07-06 20:33:37 +0530

    Project navbar

---
+++ Project_Myntra/myntra/app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepg(request):
+    return render(request, "homepg.html")
+
+def men(request):
+    return render(request, "men.html")
+
+def women(request):
+    return render(request, "women.html")
+
+def kids(request):
+    return render(request, "kids.html")
+
+def home(request):
+    return render(request, "home.html")
+
+def beauty(request):
+    return render(request, "beauty.html")
+
+def genz(request):
+    return render(request, "genz.html")
+
+def studio(request):
+    return render(request, "studio.html")
+
+def wishlist(request):
+    return render(request, "wishlist.html")
+
+def bag(request):
+    return render(request, "bag.html")
+
---
+++ Project_Myntra/myntra/myntra/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for myntra project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.homepg, name="homepg"),
+    path("men/",views.men, name='men'),
+    path("women/",views.women, name='women'),
+    path("kids/",views.kids, name='kids'),
+    path("home/",views.home, name='home'),
+    path("beauty/",views.beauty, name='beauty'),
+    path("genz/",views.genz, name='genz'),
+    path("studio/",views.studio, name='studio'),
+    path("wishlist/",views.wishlist, name='wishlist'),
+    path("bag/",views.bag, name='bag'),
+]

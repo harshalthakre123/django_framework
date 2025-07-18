9f2abeaa5ce11b9d0d7958ba0bc3210b43f74f77
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-07-18 16:49:46 +0530

    Model Relations

---
+++ Model_Relation/project/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Aadhar(models.Model):
+    a_no=models.IntegerField(unique=True)
+    gen_date=models.DateField()
+
+class Students(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    city=models.CharField()
+    a_no=models.OneToOneField(Aadhar, on_delete=models.PROTECT, related_name='stu_info')

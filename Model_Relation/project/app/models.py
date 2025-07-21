947f460dfd03b03a5f4f82e0ac6f45633e08abf9
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-07-21 19:32:21 +0530

    many to many relation concept

--- Model_Relation/project/app/models.py
+++ Model_Relation/project/app/models.py
@@ -11,3 +11,25 @@ class Students(models.Model):
     email=models.EmailField()
     city=models.CharField()
     a_no=models.OneToOneField(Aadhar, on_delete=models.PROTECT, related_name='stu_info')
+
+
+#one to many==================
+class Department(models.Model):
+    d_name=models.CharField(max_length=50)
+
+class Students1(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    depart=models.ForeignKey(Department, on_delete=models.PROTECT, related_name="student_info")
+
+
+
+# Many to many relation================================================
+
+class Fuel(models.Model):
+    f_name=models.CharField(max_length=50, unique=True)
+
+class Vehicle(models.Model):
+    v_name=models.CharField(max_length=50)
+    f_name=models.ManyToManyField(Fuel, related_name="vehicle")
--- Model_Relation/project/app/views.py
+++ Model_Relation/project/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from app.models import Students as st
 from app.models import Aadhar as aa
 from django.shortcuts import HttpResponse
+from app.models import Department as dp
+from app.models import Students1 as stu
 
 # Create your views here.
 
@@ -9,9 +11,8 @@ from django.shortcuts import HttpResponse
 
 def ForwardAccess(req):
     
-    ##one-to-one
+    ##one-to-one===============================================================
     
-    data=st.objects.all()
     # data=st.objects.get(id=1)
     # name=data.name
     # email=data.email
@@ -20,14 +21,29 @@ def ForwardAccess(req):
     # aadhar=data.a_no.a_no
     # gen_date=data.a_no.gen_date
     # return HttpResponse({name:name, email:email, city:city, aad_obj:aad_obj, aadhar:aadhar, gen_date:gen_date})
+    
+    # data=st.objects.all()
+    # return render(req, "home.html" , {'x': data})
 
-    return render(req, "home.html" , {'x': data})
+    ##one to many=====================================================================
+
+    # data1=stu.objects.get(id=1)
+    # name=data1.name
+    # email=data1.email
+    # contact=data1.contact
+    # depart_obj=data1.depart
+    # department= data1.depart.d_name
+    # return HttpResponse({name: name, email: email, contact: contact, depart_obj: depart_obj  ,department: department})
+
+    data1=stu.objects.all()
+    return render (req, 'home.html', {"a": data1})
+    
+
 
 def ReverseAccess(req):
     
     ##one-to-one
 
-    data=aa.objects.all()
     # data=aa.objects.get(id=1)
     # aadhar_no=data.a_no
     # gen_date=data.gen_date
@@ -36,4 +52,24 @@ def ReverseAccess(req):
     # email=stu_data.email
     # city=stu_data.city
     # return HttpResponse({aadhar_no:aadhar_no, gen_date:gen_date , name:name, email:email, city:city})
-    return render(req, "home.html", {'y': data})
+
+    # data=aa.objects.all()
+    # return render(req, "home.html", {'y': data})
+
+
+    ##one to many=============================================================
+
+
+    data1=dp.objects.get(id=1)
+    depart=data1.d_name
+    print(depart)
+    x=data1.student_info.all()
+    for i in x:
+        print(i.name)
+
+
+    return HttpResponse({depart: depart, name: name, email: email , contact: contact})
+
+
+    # data1=dp.objects.all()
+    # return render(req, "home.html", {"b": data1})

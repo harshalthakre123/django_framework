b1be409d2668447832301565274e297abe765264
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-08-02 15:17:26 +0530

    Regisration completed

--- Register(signup_login)/project/app/views.py
+++ Register(signup_login)/project/app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from app.models import Student as st
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -14,5 +16,49 @@ def register(req):
         d=req.FILES.get("document")
         p=req.POST.get("password")
         cp=req.POST.get("cpassword")
-        print(n, e, c, i, d, p, cp)
+        # print(n, e, c, i, d, p, cp)
+        user=st.objects.filter(email=e)
+        if user:
+            msg="user already exist!"
+            return render(req, 'register.html', {"msg":msg})
+        else:
+            if p==cp:
+                data={"name":n, "email":e, "contact":c, "image":i, "document":d, "password":p}
+                st.objects.create(name=n, email=e, contact=c, image=i, document=d, password=p)
+                return redirect("login")
+            else:
+                pmsg="password and confirm password not matched"
+                return render(req, "register.html",{"pmsg", pmsg})
     return render(req, "register.html")
+
+
+from urllib.parse import urlencode
+from django.urls import reverse
+def login(req):
+    if req.method=="POST":
+        e=req.POST.get("email")
+        p=req.POST.get("password")
+        # print(e, p, sep=",")
+        user=st.objects.filter(email=e)
+        if user:
+            data=st.objects.get(email=e)
+            upass=data.password
+            print(upass)
+            if upass==p:
+                url= reverse('dashboard')
+                data= urlencode({'id':data.id})
+                return redirect(f'{url}?{data}')
+            else:
+                pmsg="Email and Password not matched"
+                return render(req, "login.html", {"pmsg":pmsg, "email":e})
+            
+        else:
+            return redirect("register")
+    return render(req, "login.html")
+
+
+def dashboard(req):
+    pk= req.GET.get('id')
+    user= st.objects.get(id=pk)
+    data={"name":user.name, "email":user.email, "contact":user.contact, "image":user.image, "document":user.document, "password":user.password}
+    return render(req, "dashboard.html", {"data":data})
--- Register(signup_login)/project/project/urls.py
+++ Register(signup_login)/project/project/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
-    path("register/", views.register, name="register")
-]
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

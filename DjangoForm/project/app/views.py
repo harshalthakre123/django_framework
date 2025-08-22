27cb5a4e7a3d7a195acb3e4b74863469ece154c7
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-08-22 19:27:49 +0530

    Django form

--- DjangoForm/project/app/views.py
+++ DjangoForm/project/app/views.py
@@ -6,5 +6,16 @@ def home(req):
     return render(req, "home.html")
 
 def register(req):
-    fm=RegisterForm()
-    return render(req, "home.html", {"register":"register","x": fm})
+    if req.method=="POST":
+        form=RegisterForm(req.POST, req.FILES)
+        print(form)
+        if form.is_valid():
+            n=form.cleaned_data["name"]
+            e=form.cleaned_data["email"]
+            c=form.cleaned_data["contact"]
+            i=form.cleaned_data["image"]
+            f=form.cleaned_data["file"]
+            print(n, e, c, i, f, sep=",")
+    else:
+        fm=RegisterForm()
+        return render(req, "home.html", {"register":"register","x": fm})
